3ab2502117914855dc76f0bc9208c368379d814f
Ben Weier <ben.weier@gmail.com>
2016-06-21 21:24:24 +1000

    Update code standards and indentation

--- test/test.js
+++ test/test.js
@@ -2,207 +2,218 @@
 var host = 'xxx.firebaseio.com';
 var authToken = 'xxxxx';
 
-var should = require('should'),
-    session = require('express-session'),
-    FirebaseStore = require(__dirname + '/../lib/connect-firebase.js')(session);
+var should = require('should');
+var session = require('express-session');
+var FirebaseStore = require(__dirname + '/../lib/connect-firebase.js')(session);
 
 describe('FirebaseStore', function () {
-    this.timeout(0);
+  this.timeout(0);
 
-    describe('Instantiation', function () {
-        it('should be able to be created', function () {
-            var store = new FirebaseStore({
-                host: host,
-                token: authToken
-            });
-            store.should.be.an.instanceOf(FirebaseStore);
-        });
+  describe('Instantiation', function () {
+    it('should be able to be created', function () {
+      var store = new FirebaseStore({
+        host: host,
+        token: authToken
+      });
+
+      store.should.be.an.instanceOf(FirebaseStore);
     });
-    describe('Setting', function () {
-        it('should store data correctly', function (done) {
-            var store = new FirebaseStore({
-                host: host
-            });
-            store.set('1234_#$[]', {
-                cookie: {
-                    maxAge: 2000
-                },
-                name: 'tj'
-            }, function (err, res) {
-                if (err) throw err;
+  });
 
-                done();
-            });
-        });
+  describe('Setting', function () {
+    it('should store data correctly', function (done) {
+      var store = new FirebaseStore({
+        host: host
+      });
 
+      store.set('1234_#$[]', {
+        cookie: {
+          maxAge: 2000
+        },
+        name: 'tj'
+      }, function (err, res) {
+        if (err) throw err;
+
+        done();
+      });
     });
-    describe('Getting', function () {
-        before(function () {
-            var store = new FirebaseStore({
-                host: host
-            });
-            store.set('1234', {
-                cookie: {
-                    maxAge: 2000
-                },
-                name: 'tj'
-            }, function () {});
-        });
+  });
 
-        it('should get data correctly', function (done) {
-            var store = new FirebaseStore({
-                host: host
-            });
-            store.get('1234', function (err, res) {
-                if (err) throw err;
-                res.cookie.should.eql({
-                    maxAge: 2000
-                });
-                res.name.should.eql('tj');
+  describe('Getting', function () {
+    before(function () {
+      var store = new FirebaseStore({
+        host: host
+      });
 
-                done();
-            });
-        });
-
+      store.set('1234', {
+        cookie: {
+          maxAge: 2000
+        },
+        name: 'tj'
+      }, function () {});
     });
-    describe('Destroying', function () {
-        before(function () {
-            var store = new FirebaseStore({
-                host: host
-            });
-            store.set('12345', {
-                cookie: {
-                    maxAge: 2000
-                },
-                name: 'tj'
-            }, function () {});
+
+    it('should get data correctly', function (done) {
+      var store = new FirebaseStore({
+        host: host
+      });
+
+      store.get('1234', function (err, res) {
+        if (err) throw err;
+        res.cookie.should.eql({
+          maxAge: 2000
         });
+        res.name.should.eql('tj');
 
-        it('should destroy data correctly', function (done) {
-            var store = new FirebaseStore({
-                host: host
-            });
-            store.destroy('12345', function (err, res) {
-                if (err) throw err;
-
-                store.get('12345', function (err, res) {
-                    if (err) throw err;
-                    should.not.exist(res);
-
-                    done();
-                });
-            });
-        });
-
+        done();
+      });
     });
-    describe('Clearing', function () {
-        before(function () {
-            var store = new FirebaseStore({
-                host: host
-            });
-            store.set('abcd', {
-                cookie: {
-                    maxAge: 2000
-                },
-                name: 'tj'
-            }, function () {});
-            store.set('abcdef', {
-                cookie: {
-                    maxAge: 2000
-                },
-                name: 'tj'
-            }, function () {});
-        });
+  });
 
-        it('should clear sessions correctly', function (done) {
-            var store = new FirebaseStore({
-                host: host
-            });
-            store.clear(function (err, res) {
-                if (err) throw err;
+  describe('Destroying', function () {
+    before(function () {
+      var store = new FirebaseStore({
+        host: host
+      });
 
-                store.get('abcd', function (err, res) {
-                    if (err) throw err;
-                    should.not.exist(res);
-
-                    store.get('abcdef', function (err, res) {
-                        if (err) throw err;
-                        should.not.exist(res);
-
-                        done();
-                    });
-                });
-            });
-        });
-
+      store.set('12345', {
+        cookie: {
+          maxAge: 2000
+        },
+        name: 'tj'
+      }, function () {});
     });
-    describe('Reaping', function () {
-        before(function (done) {
-            var store = new FirebaseStore({
-                host: host
-            });
-            store.set('abcd', {
-                cookie: {
-                    maxAge: -2000
-                },
-                name: 'tj'
-            }, done);
+
+    it('should destroy data correctly', function (done) {
+      var store = new FirebaseStore({
+        host: host
+      });
+
+      store.destroy('12345', function (err, res) {
+        if (err) throw err;
+
+        store.get('12345', function (err, res) {
+          if (err) throw err;
+          should.not.exist(res);
+
+          done();
         });
-
-        it('should reap data correctly', function (done) {
-            var store = new FirebaseStore({
-                host: host
-            });
-            store.reap(function (err, res) {
-                if (err) throw err;
-
-                store.get('abcd', function (err, res) {
-                    if (err) throw err;
-                    should.not.exist(res);
-
-                    done();
-
-                });
-            });
-        });
-
+      });
     });
-    describe('Touching', function () {
-        before(function (done) {
-            var store = new FirebaseStore({
-                host: host
-            });
-            store.set('abcd', {
-                cookie: {
-                    maxAge: 2000
-                },
-                name: 'tj'
-            }, done);
-        });
+  });
 
-        it('should touch data correctly', function (done) {
-            var store = new FirebaseStore({
-                host: host
-            });
-            store.touch('abcd', {
-                cookie: {
-                    maxAge: 3000
-                },
-                name: 'bn'
-            }, function (err) {
-                if (err) throw err;
+  describe('Clearing', function () {
+    before(function () {
+      var store = new FirebaseStore({
+        host: host
+      });
 
-                store.get('abcd', function (err, res) {
-                    if (err) throw err;
-                    res.cookie.should.eql({
-                        maxAge: 3000
-                    });
-                    res.name.should.eql('tj');
-
-                    done();
-                });
-            });
-        });
-
+      store.set('abcd', {
+        cookie: {
+          maxAge: 2000
+        },
+        name: 'tj'
+      }, function () {});
+      store.set('abcdef', {
+        cookie: {
+          maxAge: 2000
+        },
+        name: 'tj'
+      }, function () {});
     });
+
+    it('should clear sessions correctly', function (done) {
+      var store = new FirebaseStore({
+        host: host
+      });
+
+      store.clear(function (err, res) {
+        if (err) throw err;
+
+        store.get('abcd', function (err, res) {
+          if (err) throw err;
+          should.not.exist(res);
+
+          store.get('abcdef', function (err, res) {
+            if (err) throw err;
+            should.not.exist(res);
+
+            done();
+          });
+        });
+      });
+    });
+  });
+
+  describe('Reaping', function () {
+    before(function (done) {
+      var store = new FirebaseStore({
+        host: host
+      });
+
+      store.set('abcd', {
+        cookie: {
+          maxAge: -2000
+        },
+        name: 'tj'
+      }, done);
+    });
+
+    it('should reap data correctly', function (done) {
+      var store = new FirebaseStore({
+        host: host
+      });
+
+      store.reap(function (err, res) {
+        if (err) throw err;
+
+        store.get('abcd', function (err, res) {
+          if (err) throw err;
+          should.not.exist(res);
+
+          done();
+        });
+      });
+    });
+  });
+
+  describe('Touching', function () {
+    before(function (done) {
+      var store = new FirebaseStore({
+        host: host
+      });
+
+      store.set('abcd', {
+        cookie: {
+          maxAge: 2000
+        },
+        name: 'tj'
+      }, done);
+    });
+
+    it('should touch data correctly', function (done) {
+      var store = new FirebaseStore({
+        host: host
+      });
+
+      store.touch('abcd', {
+        cookie: {
+          maxAge: 3000
+        },
+        name: 'bn'
+      }, function (err) {
+        if (err) throw err;
+
+        store.get('abcd', function (err, res) {
+          if (err) throw err;
+          res.cookie.should.eql({
+            maxAge: 3000
+          });
+          res.name.should.eql('tj');
+
+          done();
+        });
+      });
+    });
+  });
 });
