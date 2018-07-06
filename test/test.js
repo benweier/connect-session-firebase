2d22684c43e95b4edbebec35f916096a907e2afa
Ben Weier <ben.weier@gmail.com>
2018-07-06 20:17:40 +1000

    Convert tests from mocha to jest

--- test/test.js
+++ test/test.js
@@ -1,286 +1,291 @@
-/* global describe, context, it, before, after */
+const firebase = require('firebase-admin')
+const session = require('express-session')
+const FirebaseStore = require('../lib/connect-session-firebase.js')(session)
 
-const path = require('path');
-const lint = require('mocha-eslint');
-const expect = require('chai').expect;
-const session = require('express-session');
-const firebase = require('firebase-admin');
-const FirebaseStore = require(path.normalize(`${__dirname}/../lib/connect-session-firebase.js`))(session);
+require('dotenv').config({
+  silent: true,
+})
 
-require('dotenv').config({ silent: true });
-
-describe('Code Standards', function () {
-  this.slow(1000);
-
-  lint(['index.js', 'lib/connect-session-firebase.js', 'test/test.js']);
-});
-
-describe('FirebaseStore', function () {
-  this.timeout(10000);
-  this.slow(5000);
-
-  before('set up', function (done) {
-    const config = {
+describe('FirebaseStore', () => {
+  beforeAll(() => {
+    this.firebase = firebase.initializeApp({
       credential: firebase.credential.cert(process.env.FIREBASE_SERVICE_ACCOUNT),
-      databaseURL: process.env.FIREBASE_DATABASE_URL
-    };
-
-    this.firebase = firebase.initializeApp(config);
-
+      databaseURL: process.env.FIREBASE_DATABASE_URL,
+    })
     this.store = new FirebaseStore({
-      database: this.firebase.database()
-    });
+      database: this.firebase.database(),
+    })
+  })
 
-    done();
-  });
+  afterAll(() => {
+    this.firebase.delete()
+  })
 
-  after('tear down', function (done) {
-    this.firebase.delete();
-    done();
-  });
+  it('should return an instance of FirebaseStore when passed a Firebase app', () => {
+    const store = new FirebaseStore({
+      database: this.firebase,
+    })
 
-  context('when passed a valid firebase app', function () {
-    it('should be an instance of FirebaseStore', function (done) {
-      const store = new FirebaseStore({ database: this.firebase });
+    expect(store).toBeInstanceOf(FirebaseStore)
+  })
 
-      expect(store).to.be.instanceof(FirebaseStore);
-      done();
-    });
-  });
+  it('should return an instance of FirebaseStore when passed a Firebase database', () => {
+    const store = new FirebaseStore({
+      database: this.firebase.database(),
+    })
 
-  context('when passed a valid firebase database', function () {
-    it('should be an instance of FirebaseStore', function (done) {
-      const store = new FirebaseStore({ database: this.firebase.database() });
+    expect(store).toBeInstanceOf(FirebaseStore)
+  })
 
-      expect(store).to.be.instanceof(FirebaseStore);
-      done();
-    });
-  });
+  it('should set a reap interval', () => {
+    const validInterval = new FirebaseStore({
+      database: this.firebase.database(),
+      reapInterval: 86400,
+    })
+    const invalidInterval = new FirebaseStore({
+      database: this.firebase.database(),
+      reapInterval: 'never',
+    })
 
-  context('when passed valid arguments', function () {
-    it('should be an instance of FirebaseStore', function (done) {
-      expect(this.store).to.be.instanceof(FirebaseStore);
-      done();
-    });
-  });
+    expect(validInterval).toEqual(
+      expect.objectContaining({
+        reapInterval: 86400,
+      }),
+    )
+    expect(invalidInterval).toEqual(
+      expect.objectContaining({
+        reapInterval: 21600000,
+      }),
+    )
+  })
 
-  context('when passed an invalid database', function () {
-    const tests = [
-      { key: 'object', args: { database: {} } },
-      { key: 'array', args: { database: [] } },
-      { key: 'string', args: { database: '' } }
-    ];
+  it('should set a reap callback', () => {
+    const reapCallback = jest.fn()
+    const validCallback = new FirebaseStore({
+      database: this.firebase.database(),
+      reapCallback,
+    })
+    const invalidCallback = new FirebaseStore({
+      database: this.firebase.database(),
+      reapCallback: null,
+    })
 
-    tests.forEach(function (test) {
-      it(`${JSON.stringify(test.key)} should throw an error`, function (done) {
-        expect(() => new FirebaseStore(test.args)).to.throw(Error);
-        done();
-      });
-    });
-  });
+    expect(validCallback).toEqual(
+      expect.objectContaining({
+        reapCallback,
+      }),
+    )
+    expect(invalidCallback).toEqual(
+      expect.objectContaining({
+        reapCallback: expect.any(Function),
+      }),
+    )
+  })
 
-  context('when passed invalid arguments', function () {
-    const tests = [
-      { key: 'object', args: {} },
-      { key: 'array', args: [] },
-      { key: 'string', args: '' }
-    ];
+  it('should throw an error when passed invalid arguments', () => {
+    expect(() => new FirebaseStore()).toThrow('Invalid FirebaseStore argument')
+    expect(() => new FirebaseStore({})).toThrow('Invalid FirebaseStore argument')
+    expect(() => new FirebaseStore({ database: [] })).toThrow('Invalid Firebase reference')
+    expect(() => new FirebaseStore({ database: {} })).toThrow('Invalid Firebase reference')
+  })
 
-    tests.forEach(function (test) {
-      it(`${JSON.stringify(test.key)} should throw an error`, function (done) {
-        expect(() => new FirebaseStore(test.args)).to.throw(Error);
-        done();
-      });
-    });
-  });
-
-  describe('.set()', function () {
-    it('should save a session', function (done) {
-
-      Promise.all([
-        this.store.set('set-1', { name: 'tj', maxAge: 10000 }),
-        this.store.set('set-2', { name: 'tj', maxAge: 20000 })
+  describe('.set()', () => {
+    it('should save a session', async () => {
+      const sessions = await Promise.all([
+        this.store.set('set-1', {
+          name: 'tj',
+          maxAge: 10000,
+        }),
+        this.store.set('set-2', {
+          name: 'tj',
+          maxAge: 20000,
+        }),
       ])
-        .then(() => done());
+        .then(() => true)
+        .catch(() => false)
 
-    });
-  });
+      expect(sessions).toBe(true)
+    })
+  })
 
-  describe('.get()', function () {
-    before('save sessions', function (done) {
-
+  describe('.get()', () => {
+    beforeAll(() =>
       Promise.all([
-        this.store.set('get-1', { name: 'tj', cookie: { maxAge: 10000 } }, (err, first) => first),
-        this.store.set('get-2', { name: 'tj', cookie: { maxAge: -20000 } }, (err, second) => second)
+        this.store.set(
+          'get-1',
+          {
+            name: 'tj',
+            cookie: { maxAge: 10000 },
+          },
+          (err, first) => first,
+        ),
+        this.store.set(
+          'get-2',
+          {
+            name: 'tj',
+            cookie: { maxAge: -20000 },
+          },
+          (err, second) => second,
+        ),
+      ]))
+
+    it('should return an active session', async () => {
+      const sessions = await Promise.resolve(this.store.get('get-1', (err, first) => first))
+
+      expect(sessions).toEqual({
+        name: 'tj',
+        cookie: {
+          maxAge: 10000,
+        },
+      })
+    })
+
+    it('should remove an expired session', async () => {
+      const sessions = await Promise.resolve(this.store.get('get-2', (err, second) => second))
+
+      expect(sessions).toBeUndefined()
+    })
+  })
+
+  describe('.destroy()', () => {
+    beforeAll(async () => {
+      await Promise.all([
+        this.store.set('destroy-1', {
+          name: 'tj',
+          cookie: { maxAge: 10000 },
+        }),
+        this.store.set('destroy-2', {
+          name: 'tj',
+          cookie: { maxAge: 20000 },
+        }),
       ])
-        .then(() => done());
 
-    });
+      await this.store.destroy('destroy-1')
+    })
 
-    it('should fetch a session', function (done) {
+    it('should remove a session', async () => {
+      await this.store.destroy('destroy-1')
 
+      const sessions = await Promise.all([
+        this.store.get('destroy-1', (err, first) => first),
+        this.store.get('destroy-2', (err, second) => second),
+      ])
+
+      const [first, second] = sessions
+
+      expect(first).toBeUndefined()
+      expect(second).toEqual({
+        name: 'tj',
+        cookie: {
+          maxAge: 20000,
+        },
+      })
+    })
+  })
+
+  describe('.reap()', () => {
+    beforeAll(() =>
       Promise.all([
-        this.store.get('get-1', (err, first) => first)
+        this.store.set('reap-1', {
+          name: 'tj',
+          cookie: { maxAge: -10000 },
+        }),
+        this.store.set('reap-2', {
+          name: 'tj',
+          cookie: { maxAge: 20000 },
+        }),
+        this.store.set('reap-3', {
+          name: 'tj',
+          cookie: { maxAge: -30000 },
+        }),
+      ]))
+
+    it('should remove all expired sessions', async () => {
+      await this.store.reap()
+
+      const sessions = await Promise.all([
+        this.store.get('reap-1', (err, first) => first),
+        this.store.get('reap-2', (err, second) => second),
+        this.store.get('reap-3', (err, third) => third),
       ])
-        .then(sessions => {
-          const first = sessions[0];
 
-          expect(first).to.exist.and.to.have.property('cookie').and.to.have.property('maxAge').and.to.eql(10000);
+      const [first, second, third] = sessions
 
-        })
-        .then(() => done());
+      expect(first).toBeUndefined()
+      expect(second).toEqual({
+        name: 'tj',
+        cookie: {
+          maxAge: 20000,
+        },
+      })
+      expect(third).toBeUndefined()
+    })
+  })
 
-    });
-
-    it('should remove an expired session', function (done) {
-
+  describe('.touch()', () => {
+    beforeAll(() =>
       Promise.all([
-        this.store.get('get-2', (err, second) => second)
+        this.store.set('touch-1', {
+          name: 'tj',
+          cookie: { maxAge: 10000 },
+        }),
+        this.store.set('touch-2', {
+          name: 'tj',
+          cookie: { maxAge: 20000 },
+        }),
+      ]))
+
+    it('should update a session', async () => {
+      await Promise.resolve(
+        this.store.touch('touch-1', {
+          name: 'bn',
+          cookie: { maxAge: 30000 },
+        }),
+      )
+
+      const sessions = await Promise.all([
+        this.store.get('touch-1', (err, first) => first),
+        this.store.get('touch-2', (err, second) => second),
       ])
-        .then(sessions => {
-          const second = sessions[0];
 
-          expect(second).to.not.exist;
-        })
-        .then(() => done());
+      const [first, second] = sessions
 
-    });
-  });
+      expect(first).toEqual({
+        name: 'tj',
+        cookie: { maxAge: 30000 },
+      })
+      expect(second).toEqual({
+        name: 'tj',
+        cookie: { maxAge: 20000 },
+      })
+    })
+  })
 
-  describe('.destroy()', function () {
-    before('save sessions', function (done) {
-
+  describe('.clear()', () => {
+    beforeAll(() =>
       Promise.all([
-        this.store.set('destroy-1', { name: 'tj', cookie: { maxAge: 10000 } }),
-        this.store.set('destroy-2', { name: 'tj', cookie: { maxAge: 20000 } })
+        this.store.set('clear-1', {
+          name: 'tj',
+          cookie: { maxAge: 10000 },
+        }),
+        this.store.set('clear-2', {
+          name: 'tj',
+          cookie: { maxAge: 20000 },
+        }),
+      ]))
+
+    it('should remove all sessions', async () => {
+      await this.store.clear()
+
+      const sessions = await Promise.all([
+        this.store.get('clear-1', (err, first) => first),
+        this.store.get('clear-2', (err, second) => second),
       ])
-        .then(() => done());
 
-    });
+      const [first, second] = sessions
 
-    it('should remove a session', function (done) {
-      this.store.destroy('destroy-1')
-        .then(() => {
-
-          Promise.all([
-            this.store.get('destroy-1', (err, first) => first),
-            this.store.get('destroy-2', (err, second) => second)
-          ])
-            .then(sessions => {
-              const first = sessions[0];
-              const second = sessions[1];
-
-              expect(first).to.not.exist;
-              expect(second).to.exist.and.to.have.property('cookie').and.to.have.property('maxAge').and.to.eql(20000);
-            })
-            .then(() => done());
-
-        });
-    });
-  });
-
-  describe('.reap()', function () {
-    before('save sessions', function (done) {
-
-      Promise.all([
-        this.store.set('reap-1', { name: 'tj', cookie: { maxAge: -10000 } }),
-        this.store.set('reap-2', { name: 'tj', cookie: { maxAge: 20000 } }),
-        this.store.set('reap-3', { name: 'tj', cookie: { maxAge: -30000 } })
-      ])
-        .then(() => done());
-
-    });
-
-    it('should remove all expired sessions', function (done) {
-      this.store.reap()
-        .then(() => {
-
-          Promise.all([
-            this.store.get('reap-1', (err, first) => first),
-            this.store.get('reap-2', (err, second) => second),
-            this.store.get('reap-3', (err, third) => third)
-          ])
-            .then(sessions => {
-              const first = sessions[0];
-              const second = sessions[1];
-              const third = sessions[2];
-
-              expect(first).to.not.exist;
-              expect(second).to.exist.and.to.have.property('cookie').and.to.have.property('maxAge').and.to.eql(20000);
-              expect(third).to.not.exist;
-            })
-            .then(() => done());
-
-        });
-    });
-  });
-
-  describe('.touch()', function () {
-    before('save sessions', function (done) {
-
-      Promise.all([
-        this.store.set('touch-1', { name: 'tj', cookie: { maxAge: 10000 } }),
-        this.store.set('touch-2', { name: 'tj', cookie: { maxAge: 20000 } })
-      ])
-        .then(() => done());
-
-    });
-
-    it('should update a session', function (done) {
-
-      this.store.touch('touch-1', { name: 'bn', cookie: { maxAge: 30000 } })
-        .then(() => {
-
-          Promise.all([
-            this.store.get('touch-1', (err, first) => first),
-            this.store.get('touch-2', (err, second) => second)
-          ])
-            .then(sessions => {
-              const first = sessions[0];
-              const second = sessions[1];
-
-              expect(first).to.exist.and.to.have.property('name').and.to.eql('tj');
-              expect(first).to.exist.and.to.have.property('cookie').and.to.have.property('maxAge').and.to.eql(30000);
-
-              expect(second).to.exist.and.to.have.property('name').and.to.eql('tj');
-              expect(second).to.exist.and.to.have.property('cookie').and.to.have.property('maxAge').and.to.eql(20000);
-            })
-            .then(() => done());
-
-        });
-
-    });
-  });
-
-  describe('.clear()', function () {
-    before('save sessions', function (done) {
-
-      Promise.all([
-        this.store.set('clear-1', { name: 'tj', cookie: { maxAge: 10000 } }),
-        this.store.set('clear-2', { name: 'tj', cookie: { maxAge: 20000 } })
-      ])
-        .then(() => done());
-
-    });
-
-    it('should remove all sessions', function (done) {
-      this.store.clear()
-        .then(() => {
-
-          Promise.all([
-            this.store.get('clear-1', (err, first) => first),
-            this.store.get('clear-2', (err, second) => second)
-          ])
-            .then(sessions => {
-              const first = sessions[0];
-              const second = sessions[1];
-
-              expect(first).to.not.exist;
-              expect(second).to.not.exist;
-            })
-            .then(() => done());
-
-        });
-    });
-  });
-});
+      expect(first).toBeUndefined()
+      expect(second).toBeUndefined()
+    })
+  })
+})
