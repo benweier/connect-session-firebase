471b78351540a474d8a9dd431bccca97a3a3fd9d
Ben Weier <ben.weier@gmail.com>
2023-03-02 20:07:41 +1000

    fix env var object

--- test/test.js
+++ test/test.js
@@ -11,7 +11,7 @@ describe('FirebaseStore', () => {
     this.firebase = firebase.initializeApp({
       credential: firebase.credential.cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.FIREBASE_CLIENT_EMAIL,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
         privateKey: process.env.FIREBASE_PRIVATE_KEY,
       }),
       databaseURL: process.env.FIREBASE_DATABASE_URL,
